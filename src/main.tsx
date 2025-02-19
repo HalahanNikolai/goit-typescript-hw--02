941c809f49c44a53347513b52b95216d17dee66d
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2025-02-20 00:30:27 +0200

    init1

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 // import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "./index.css";
 // import App from './App.jsx'
 
 // createRoot(document.getElementById('root')).render(
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
